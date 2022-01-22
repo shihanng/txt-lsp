03a632fe144b451f5e9f5aa6ac957895a951492d
Shi Han NG <shihanng@gmail.com>
2022-01-22 16:10:48 +0900

    Implement simple not functional LSP

--- .vimrc.lua
+++ .vimrc.lua
@@ -4,7 +4,7 @@ local configs = require("lspconfig.configs")
 if not configs.foo_lsp then
 	configs.foo_lsp = {
 		default_config = {
-			cmd = { "/home/neovim/lua-language-server/run.sh" },
+			cmd = { "./txt-lsp" },
 			filetypes = { "text" },
 			root_dir = function(fname)
 				return lspconfig.util.find_git_ancestor(fname)
--- main.go
+++ main.go
@@ -1,10 +1,48 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"os"
+
+	"github.com/cockroachdb/errors"
+	"github.com/sourcegraph/jsonrpc2"
 )
 
+var errNotImplemented = errors.New("not implemented")
+
+func handle(ctx context.Context, conn *jsonrpc2.Conn, req *jsonrpc2.Request) (result interface{}, err error) {
+	return nil, errNotImplemented
+}
+
 func main() {
-	fmt.Fprintf(os.Stdout, "hello lsp")
+	handler := jsonrpc2.HandlerWithError(handle)
+
+	connOpt := []jsonrpc2.ConnOpt{}
+
+	<-jsonrpc2.NewConn(
+		context.Background(),
+		jsonrpc2.NewBufferedStream(stdrwc{}, jsonrpc2.VSCodeObjectCodec{}),
+		handler, connOpt...).DisconnectNotify()
+}
+
+type stdrwc struct{}
+
+func (stdrwc) Read(p []byte) (int, error) {
+	n, err := os.Stdin.Read(p)
+
+	return n, errors.Wrap(err, "main: read from stdin")
+}
+
+func (c stdrwc) Write(p []byte) (int, error) {
+	n, err := os.Stdout.Write(p)
+
+	return n, errors.Wrap(err, "main: write to stdout")
+}
+
+func (c stdrwc) Close() error {
+	if err := os.Stdin.Close(); err != nil {
+		return errors.Wrap(err, "main: close stdin")
+	}
+
+	return errors.Wrap(os.Stdout.Close(), "main: close stdout")
 }
